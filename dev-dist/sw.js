8f72ab28de77ee1a1459663b0a456ed0da9afb0a
kidmortal <kidmortal@gmail.com>
2024-02-24 18:01:13 -0300

    add well known asset

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kqpk9o45nrg"
+    "revision": "0.t8jfvbhr2og"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
